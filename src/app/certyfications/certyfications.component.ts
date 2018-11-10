410fccec75938cebf9df101c7cb51d9502b36432
Piotr Kulpa <p.kulpa@onet.eu>
2018-11-10 11:02:34 +0100

    feat: add html update to certifications component

--- src/app/certyfications/certyfications.component.ts
+++ src/app/certyfications/certyfications.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdecticaService } from '../prodentica.service';
 
 @Component({
   selector: 'app-certyfications',
   templateUrl: './certyfications.component.html',
-  styleUrls: ['./certyfications.component.css']
+  styleUrls: ['./certyfications.component.css'],
+  providers: [ProdecticaService]
 })
 /** Class representing a Certyfications component. */
 export class CertyficationsComponent implements OnInit {
 
   certs: any[] = [];
+  certKopec: any[] = [];
+  certRadej: any[] = [];
   case: number = 1;
 
-  constructor() { }
+  constructor(private prodecticaService: ProdecticaService) { }
 
   ngOnInit() {
+
+    /** Set Dr Kopec thumbnails. */
+    this.certKopec = this.prodecticaService.certKopec;
+
+    /** Set Dr Radej thumbnails. */
+    this.certRadej = this.prodecticaService.certRadej;
 
     /** Set number of certification thumbnails. */
     for (var i = 1; i <= 70; i++) {
---
+++ src/app/prodentica.service.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+export class ProdecticaService {
+
+  certKopec = [
+    {id: 1, name: "bhp"},
+    {id: 2, name: "czysterece"},
+    {id: 3, name: "dylematy"},
+    {id: 4, name: "endo"},
+    {id: 5, name: "materialy"},
+    {id: 6, name: "mip"},
+    {id: 7, name: "mip2"},
+    {id: 8, name: "mp"},
+    {id: 9, name: "mp2"},
+    {id: 10, name: "mp3"},
+    {id: 11, name: "odbudowa"},
+    {id: 12, name: "organizacja"},
+    {id: 13, name: "prace"},
+    {id: 14, name: "promo"},
+    {id: 15, name: "ratownictwo"},
+    {id: 16, name: "rtg"},
+    {id: 17, name: "rtg2"},
+    {id: 18, name: "saf"},
+    {id: 19, name: "tkankiokw"},
+    {id: 20, name: "zdrowie"}
+  ];
+
+  certRadej = [
+    {id: 1, name: "IMG_20180102_0001_NEW"},
+    {id: 2, name: "IMG_20180102_0003_NEW"},
+    {id: 3, name: "IMG_20180102_0003_NEW_0001"},
+    {id: 4, name: "IMG_20180102_0003_NEW_0002"},
+    {id: 5, name: "IMG_20180102_0005_NEW"},
+    {id: 6, name: "IMG_20180102_0005_NEW_0001"},
+    {id: 7, name: "IMG_20180102_0005_NEW_0002"},
+    {id: 8, name: "IMG_20180102_0005_NEW_0003"},
+    {id: 9, name: "IMG_20180102_0005_NEW_0004"},
+    {id: 10, name: "IMG_20180102_0006_NEW"},
+    {id: 11, name: "IMG_20180102_0007_NEW"},
+    {id: 12, name: "IMG_20180102_0007_NEW_0001"},
+    {id: 13, name: "IMG_20180102_0007_NEW_0002"},
+    {id: 14, name: "IMG_20180102_0007_NEW_0003"},
+    {id: 15, name: "IMG_20180102_0007_NEW_0004"},
+    {id: 16, name: "IMG_20180102_0007_NEW_0005"},
+    {id: 17, name: "IMG_20180102_0008_NEW"},
+    {id: 18, name: "IMG_20180102_0008_NEW_0001"},
+    {id: 19, name: "IMG_20180102_0008_NEW_0002"}
+  ];
+}
